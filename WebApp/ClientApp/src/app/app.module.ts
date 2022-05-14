d43c0d3fa82a6bcf18025411612b791dc7d88e2c
Tommy Kovalchuk <tommy.pk17@gmail.com>
2022-05-14 10:57:31 -0400

    Add MSAL to WebApp

--- WebApp/ClientApp/src/app/app.module.ts
+++ WebApp/ClientApp/src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MsalBroadcastService, MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent, MsalService} from "@azure/msal-angular";
+import {BrowserCacheLocation, InteractionType, PublicClientApplication} from "@azure/msal-browser";
+import {environment} from "../environments/environment";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,9 +14,43 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MsalModule.forRoot( new PublicClientApplication({ // MSAL Configuration
+      auth: {
+        clientId: environment.AzureAd.clientId,
+        authority: environment.AzureAd.authority,
+        redirectUri: environment.AzureAd.redirectUri
+      },
+      cache: {
+        cacheLocation : BrowserCacheLocation.LocalStorage,
+        storeAuthStateInCookie: true, // set to true for IE 11
+      },
+      system: {
+        loggerOptions: {
+          loggerCallback: () => {},
+          piiLoggingEnabled: false
+        }
+      }
+    }), {
+      interactionType: InteractionType.Redirect, // MSAL Guard Configuration
+    }, {
+      interactionType: InteractionType.Redirect,
+      protectedResourceMap: new Map([
+        ['https://localhost:5001/api/*', ['API.Read']],
+      ])})
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
