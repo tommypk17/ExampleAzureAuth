af610418d3e07322dd7b4a3cca7a471ab8359090
Tommy Kovalchuk <tommy.pk17@gmail.com>
2022-05-14 09:51:12 -0400

    Add initial API with Azure AD Auth

---
+++ API/Controllers/FilesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var res = new string[]{"Yellow", "Green", "Blue"};
+            return StatusCode(StatusCodes.Status200OK, res);
+        }
+    }
+}
